8806fa9b1d4772f331f40a7ce55e588615b86242
Prafful Kumar <92020279+Pk7372singh@users.noreply.github.com>
2024-09-15 02:36:09 +0530

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://13.60.215.210:8080",
+  baseURL: "http://13.60.215.210:5000",
   headers: {
     "Content-Type": "application/json",
   },
